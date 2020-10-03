5a9009ca237051ba0e9d4073c039a6a47e061ab4
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-10-03 19:18:37 +0200

    remove individual editorconfig, gitattributes and gitignore, reformat

--- mono/dodge_the_creeps/Main.cs
+++ mono/dodge_the_creeps/Main.cs
@@ -3,88 +3,87 @@ using System;
 
 public class Main : Node
 {
-    [Export]
-    public PackedScene mob;
+  [Export] public PackedScene mob;
 
-    private int _score;
+  private int _score;
 
-    // We use 'System.Random' as an alternative to GDScript's random methods.
-    private readonly Random _random = new Random();
+  // We use 'System.Random' as an alternative to GDScript's random methods.
+  private readonly Random _random = new Random();
 
-    // We'll use this later because C# doesn't support GDScript's randi().
-    private float RandRange(float min, float max)
-    {
-        return (float)_random.NextDouble() * (max - min) + min;
-    }
+  // We'll use this later because C# doesn't support GDScript's randi().
+  private float RandRange(float min, float max)
+  {
+    return (float) _random.NextDouble() * (max - min) + min;
+  }
 
-    public void GameOver()
-    {
-        GetNode<Timer>("MobTimer").Stop();
-        GetNode<Timer>("ScoreTimer").Stop();
+  public void GameOver()
+  {
+    GetNode<Timer>("MobTimer").Stop();
+    GetNode<Timer>("ScoreTimer").Stop();
 
-        GetNode<HUD>("HUD").ShowGameOver();
+    GetNode<HUD>("HUD").ShowGameOver();
 
-        GetNode<AudioStreamPlayer>("Music").Stop();
-        GetNode<AudioStreamPlayer>("DeathSound").Play();
-    }
+    GetNode<AudioStreamPlayer>("Music").Stop();
+    GetNode<AudioStreamPlayer>("DeathSound").Play();
+  }
 
-    public void NewGame()
-    {
-        _score = 0;
+  public void NewGame()
+  {
+    _score = 0;
 
-        var player = GetNode<Player>("Player");
-        var startPosition = GetNode<Position2D>("StartPosition");
-        player.Start(startPosition.Position);
+    var player = GetNode<Player>("Player");
+    var startPosition = GetNode<Position2D>("StartPosition");
+    player.Start(startPosition.Position);
 
-        GetNode<Timer>("StartTimer").Start();
+    GetNode<Timer>("StartTimer").Start();
 
-        var hud = GetNode<HUD>("HUD");
-        hud.UpdateScore(_score);
-        hud.ShowMessage("Get Ready!");
+    var hud = GetNode<HUD>("HUD");
+    hud.UpdateScore(_score);
+    hud.ShowMessage("Get Ready!");
 
-        GetNode<AudioStreamPlayer>("Music").Play();
-    }
+    GetNode<AudioStreamPlayer>("Music").Play();
+  }
 
-    public void OnStartTimerTimeout()
-    {
-        GetNode<Timer>("MobTimer").Start();
-        GetNode<Timer>("ScoreTimer").Start();
-    }
+  public void OnStartTimerTimeout()
+  {
+    GetNode<Timer>("MobTimer").Start();
+    GetNode<Timer>("ScoreTimer").Start();
+  }
 
-    public void OnScoreTimerTimeout()
-    {
-        _score++;
+  public void OnScoreTimerTimeout()
+  {
+    _score++;
 
-        GetNode<HUD>("HUD").UpdateScore(_score);
-    }
+    GetNode<HUD>("HUD").UpdateScore(_score);
+  }
 
-    public void OnMobTimerTimeout()
-    {
-        // Note: Normally it is best to use explicit types rather than the var keyword.
-        // However, var is acceptable to use here because the types are obviously
-        // PathFollow2D and RigidBody2D, since they appear later on the line.
+  public void OnMobTimerTimeout()
+  {
+    // Note: Normally it is best to use explicit types rather than the var keyword.
+    // However, var is acceptable to use here because the types are obviously
+    // PathFollow2D and RigidBody2D, since they appear later on the line.
 
-        // Choose a random location on Path2D.
-        var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
-        mobSpawnLocation.Offset = _random.Next();
+    // Choose a random location on Path2D.
+    var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+    mobSpawnLocation.Offset = _random.Next();
 
-        // Create a Mob instance and add it to the scene.
-        var mobInstance = (RigidBody2D)mob.Instance();
-        AddChild(mobInstance);
+    // Create a Mob instance and add it to the scene.
+    var mobInstance = (RigidBody2D) mob.Instance();
+    AddChild(mobInstance);
 
-        // Set the mob's direction perpendicular to the path direction.
-        float direction = mobSpawnLocation.Rotation + Mathf.Tau / 4;
+    // Set the mob's direction perpendicular to the path direction.
+    float direction = mobSpawnLocation.Rotation + Mathf.Tau / 4;
 
-        // Set the mob's position to a random location.
-        mobInstance.Position = mobSpawnLocation.Position;
+    // Set the mob's position to a random location.
+    mobInstance.Position = mobSpawnLocation.Position;
 
-        // Add some randomness to the direction.
-        direction += RandRange(-Mathf.Tau / 8, Mathf.Tau / 8);
-        mobInstance.Rotation = direction;
+    // Add some randomness to the direction.
+    direction += RandRange(-Mathf.Tau / 8, Mathf.Tau / 8);
+    mobInstance.Rotation = direction;
 
-        // Choose the velocity.
-        mobInstance.LinearVelocity  = new Vector2(RandRange(150f, 250f), 0).Rotated(direction);
+    // Choose the velocity.
+    mobInstance.LinearVelocity = new Vector2(RandRange(150f, 250f), 0).Rotated(direction);
 
-        GetNode<HUD>("HUD").Connect(nameof(HUD.StartGame), mobInstance, nameof(Mob.OnStartGame));
-    }
+    GetNode<HUD>("HUD").Connect(nameof(HUD.StartGame), mobInstance, nameof(Mob.OnStartGame));
+  }
 }
--- mono/dodge_the_creeps/Player.cs
+++ mono/dodge_the_creeps/Player.cs
@@ -2,71 +2,70 @@ using Godot;
 
 public class Player : Area2D
 {
-    [Signal]
-    public delegate void Hit();
+  [Signal]
+  public delegate void Hit();
 
-    [Export]
-    public int speed; // How fast the player will move (pixels/sec).
+  [Export] public int speed; // How fast the player will move (pixels/sec).
 
-    private Vector2 _screenSize; // Size of the game window.
+  private Vector2 _screenSize; // Size of the game window.
 
-    public override void _Ready()
+  public override void _Ready()
+  {
+    _screenSize = GetViewport().Size;
+    Hide();
+  }
+
+  public override void _Process(float delta)
+  {
+    Vector2 velocity; // The player's movement vector.
+    velocity.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+    velocity.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+
+    var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+
+    if (velocity.Length() > 0)
     {
-        _screenSize = GetViewport().Size;
-        Hide();
+      velocity = velocity.Normalized() * speed;
+      animatedSprite.Play();
+    }
+    else
+    {
+      animatedSprite.Stop();
     }
 
-    public override void _Process(float delta)
+    Position += velocity * delta;
+    Position = new Vector2(
+      x: Mathf.Clamp(Position.x, 0, _screenSize.x),
+      y: Mathf.Clamp(Position.y, 0, _screenSize.y)
+    );
+
+    if (velocity.x != 0)
     {
-        Vector2 velocity; // The player's movement vector.
-        velocity.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-        velocity.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
-
-        var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
-
-        if (velocity.Length() > 0)
-        {
-            velocity = velocity.Normalized() * speed;
-            animatedSprite.Play();
-        }
-        else
-        {
-            animatedSprite.Stop();
-        }
-
-        Position += velocity * delta;
-        Position = new Vector2(
-            x: Mathf.Clamp(Position.x, 0, _screenSize.x),
-            y: Mathf.Clamp(Position.y, 0, _screenSize.y)
-        );
-
-        if (velocity.x != 0)
-        {
-            animatedSprite.Animation = "right";
-            // See the note below about boolean assignment.
-            animatedSprite.FlipH = velocity.x < 0;
-            animatedSprite.FlipV = false;
-        }
-        else if (velocity.y != 0)
-        {
-            animatedSprite.Animation = "up";
-            animatedSprite.FlipV = velocity.y > 0;
-        }
+      animatedSprite.Animation = "right";
+      // See the note below about boolean assignment.
+      animatedSprite.FlipH = velocity.x < 0;
+      animatedSprite.FlipV = false;
     }
-
-    public void Start(Vector2 pos)
+    else if (velocity.y != 0)
     {
-        Position = pos;
-        Show();
-        // Must be deferred as we can't change physics properties on a physics callback.
-        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("Disabled", false);
+      animatedSprite.Animation = "up";
+      animatedSprite.FlipV = velocity.y > 0;
     }
+  }
 
-    public void OnPlayerBodyEntered(PhysicsBody2D body)
-    {
-        Hide(); // Player disappears after being hit.
-        EmitSignal(nameof(Hit));
-        // Must be deferred as we can't change physics properties on a physics callback.
-        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("Disabled", true);
-    }
+  public void Start(Vector2 pos)
+  {
+    Position = pos;
+    Show();
+    // Must be deferred as we can't change physics properties on a physics callback.
+    GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("Disabled", false);
+  }
+
+  public void OnPlayerBodyEntered(PhysicsBody2D body)
+  {
+    Hide(); // Player disappears after being hit.
+    EmitSignal(nameof(Hit));
+    // Must be deferred as we can't change physics properties on a physics callback.
+    GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("Disabled", true);
+  }
 }
