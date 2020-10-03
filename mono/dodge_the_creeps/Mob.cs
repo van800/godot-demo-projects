ceeed623fbb58086e92ec0836156300eb0418d81
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-10-03 19:23:20 +0200

    indents for C#

--- mono/dodge_the_creeps/Mob.cs
+++ mono/dodge_the_creeps/Mob.cs
@@ -3,27 +3,27 @@ using System;
 
 public class Mob : RigidBody2D
 {
-  [Export] public int minSpeed; // Minimum speed range.
+    [Export] public int minSpeed; // Minimum speed range.
 
-  [Export] public int maxSpeed; // Maximum speed range.
+    [Export] public int maxSpeed; // Maximum speed range.
 
-  private readonly string[] _mobTypes = {"walk", "swim", "fly"};
+    private readonly string[] _mobTypes = {"walk", "swim", "fly"};
 
-  // C# doesn't have GDScript's random methods, so we use System.Random instead.
-  private static readonly Random _random = new Random();
+    // C# doesn't have GDScript's random methods, so we use System.Random instead.
+    private static readonly Random _random = new Random();
 
-  public override void _Ready()
-  {
-    GetNode<AnimatedSprite>("AnimatedSprite").Animation = _mobTypes[_random.Next(0, _mobTypes.Length)];
-  }
+    public override void _Ready()
+    {
+        GetNode<AnimatedSprite>("AnimatedSprite").Animation = _mobTypes[_random.Next(0, _mobTypes.Length)];
+    }
 
-  public void OnVisibilityScreenExited()
-  {
-    QueueFree();
-  }
+    public void OnVisibilityScreenExited()
+    {
+        QueueFree();
+    }
 
-  public void OnStartGame()
-  {
-    QueueFree();
-  }
+    public void OnStartGame()
+    {
+        QueueFree();
+    }
 }
