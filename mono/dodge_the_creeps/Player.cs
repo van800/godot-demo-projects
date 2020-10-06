e58be3c5acbf27f075fe7dcc612643bd95c2e019
Aaron Franke <arnfranke@yahoo.com>
2020-10-06 04:05:51 -0400

    Mark speed and screen size as private

--- mono/dodge_the_creeps/Player.cs
+++ mono/dodge_the_creeps/Player.cs
@@ -5,6 +5,9 @@ public class Player : Area2D
     [Signal]
     public delegate void Hit();
 
+    // These only need to be accessed in this script, so we can make them private.
+    // Private variables in C# in Godot have their name starting with an
+    // underscore and also have the "private" keyword instead of "public".
     [Export]
     private int _speed = 400; // How fast the player will move (pixels/sec).
 
@@ -12,7 +15,7 @@ public class Player : Area2D
 
     public override void _Ready()
     {
-        _screenSize = GetViewport().Size;
+        _screenSize = GetViewportRect().Size;
         Hide();
     }
 
