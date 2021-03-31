088741997465f57114f016d29bbc00cd02061913
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-03-31 14:00:04 +0200

    call Quit after tests

--- mono/dodge_the_creeps/RiderTestRunner/Runner.cs
+++ mono/dodge_the_creeps/RiderTestRunner/Runner.cs
@@ -4,31 +4,31 @@ using Godot;
 using Environment = System.Environment;
 using Thread = System.Threading.Thread;
 
+// ReSharper disable once CheckNamespace
 namespace RiderTestRunner
 {
+    // ReSharper disable once UnusedType.Global
     public class Runner : Node
     {
         public override void _Ready()
         {
-            var unitTestAssemblyPrefix = "--unit_test_assembly ";
-            var unitTestArgsPrefix = "--unit_test_args ";
-
             var textNode = GetNode<RichTextLabel>("RichTextLabel");
-            foreach (var arg in  OS.GetCmdlineArgs())
+            foreach (var arg in OS.GetCmdlineArgs())
             {
                 textNode.Text += Environment.NewLine + arg;
             }
-            
-            if (OS.GetCmdlineArgs().Length <4)
+
+            if (OS.GetCmdlineArgs().Length < 4)
                 return;
 
             var unitTestAssembly = OS.GetCmdlineArgs()[2];
-            var unitTestArgs = OS.GetCmdlineArgs()[4].Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var unitTestArgs = OS.GetCmdlineArgs()[4].Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToArray();
             // https://docs.microsoft.com/en-us/dotnet/api/system.appdomain.executeassembly?view=netframework-4.7.2
             AppDomain currentDomain = AppDomain.CurrentDomain;
             var thread = new Thread(() =>
             {
                 currentDomain.ExecuteAssembly(unitTestAssembly, unitTestArgs);
+                GetTree().Quit();
             });
             thread.Start();
 
