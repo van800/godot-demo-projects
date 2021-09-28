27f8fb550795d7751d14d94a2c53a00c3d420975
Ivan.Shakhov <van800@gmail.com>
2021-09-28 16:54:43 +0200

    add XUnit test

---
+++ mono/dodge_the_creeps/projects/GodotXUnitTestProject/MyTest.cs
@@ -0,0 +1,22 @@
+using System;
+using DodgeTheCreeps;
+using Godot;
+using Xunit;
+
+namespace TestProject1
+{
+    public class MyTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            int i = 0;
+            Console.WriteLine("Testsss");
+            GD.Print("XUnitTest");
+            var main = (Main)ResourceLoader.Load<PackedScene>("res://Main.tscn").Instance();
+            GD.Print(System.Threading.Thread.CurrentThread.IsBackground);
+            //System.Environment.CurrentManagedThreadId
+            Assert.Equal("Main", main.Name);
+        }
+    }
+}
